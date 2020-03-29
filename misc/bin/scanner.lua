f7ef4ddf526ca17dc0952b6f1a81dfe9e0dfcdb2
burlada <burlada@yandex-team.ru>
2020-03-29 22:14:10 +0300

    fix

--- misc/bin/scanner.lua
+++ misc/bin/scanner.lua
@@ -8,9 +8,9 @@ local scrW, scrH = gpu.getResolution()
 local keybinds = keys.loadConfig("/etc/scanner.cfg", {
   left = {{"left"}}, right = {{"right"}}, up = {{"up"}}, down = {{"down"}}, pageUp = {{"pageUp"}}, pageDown = {{"pageDown"}},
   scanLvlDown = {{"minus"}, {"numpadsub"}}, scanLvlUp = {{"shift", "equals"}, {"numpadadd"}},
-  toggleMode = {{"space"}}, close = {{"control", "q"}}, home = {{"home"}},
+  toggleMode = {{"space"}}, close = {{"control", "q"}}, home = {{"home"}}, toggleSelf = {{"h"}},
 })
-local changed, scanMode, status = true, "none", "wait"
+local changed, scanMode, status, showSelf = true, "none", "wait", true
 local bx,by,bz = 1,1,1
 local scanW, scanD, scanLvl, scanMaxLvl, tick = 4, 2, 1, 6, 0
 local baseX = scanW*8+2
@@ -46,9 +46,9 @@ local function shift(dx,dz,dy)
   by = math.max(1,math.min(by+dy, scanner.by))
 end
 local function toggleMode()
-  if scanMode == "full" then scanMode = "window"
-  elseif scanMode == "window" then scanMode = "none"
-  else scanMode = "full" end
+  if scanMode == "none" then scanMode = "window"
+  elseif scanMode == "window" then scanMode = "full"
+  else scanMode = "none" end
 end
 local function draw(tick)
   local repr = scanner.getRepr(by,bx,bz,scanW,scanD)
@@ -57,7 +57,7 @@ local function draw(tick)
   local selfX, selfZ = -scanner.x-(bx-1)*scanner.bw, -scanner.z-(bz-1)*scanner.bd
   
   for z, line in ipairs(repr) do gpu.set(1, z, line) end
-  if selfX >= 1 and selfZ >= 1 and selfX <= scanW*8 and selfZ <= scanD*8 then gpu.set(selfX, selfZ, "@") end
+  if selfX >= 1 and selfZ >= 1 and selfX <= scanW*8 and selfZ <= scanD*8 and showSelf then gpu.set(selfX, selfZ, "@") end
   gpu.fill(baseX, 1, scrW-baseX, scrH, " ")
 
   gpu.set(baseX,1, " Mode: "..scanMode)
@@ -67,7 +67,6 @@ local function draw(tick)
   gpu.set(baseX,5, " Lvl: "..tostring(scanLvl).."->"..tostring(sizes[scanLvl]))
   gpu.set(baseX,6, " Bxyz: "..tostring(bx)..tostring(bz)..tostring(by))
   gpu.set(baseX,7, " P: "..tostring(cx).." "..tostring(cz).." "..tostring(cy))
-  gpu.set(baseX,8, " Self: "..tostring(selfX).." "..tostring(selfZ))
 end
 local handlers = {
   left = function() shift(-1, 0, 0) end,
@@ -78,6 +77,7 @@ local handlers = {
   pageDown = function() shift(0, 0, -1) end,
   scanLvlUp = function() scanLvl = math.min(scanLvl+1, scanMaxLvl) end,
   scanLvlDown = function() scanLvl = math.max(1, scanLvl-1) end,
+  toggleSelf = function() showSelf = not showSelf end,
   home = home,
   close = close,
   toggleMode = toggleMode,
