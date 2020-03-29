3f46ee4232b81dc7e7a62726b86f6f10baadab29
burlada <burlada@yandex-team.ru>
2020-03-30 01:05:48 +0300

    fake

--- misc/bin/scanner.lua
+++ misc/bin/scanner.lua
@@ -55,13 +55,12 @@ local function shift(dx,dz,dy)
   bx,bz,by=bx+dx,bz+dz,by+dy
   normalize()
 end
-local function activate(ny)
+local function activate(dy)
   if ny == scanner.y then return end
-  local x, z, w, d = scanner.x, scanner.z, scanner.w, scanner.d
-  fakeLevels[scanner.y+ny-1] = nil
+  local x, z, y, w, d = scanner.x, scanner.z, scanner.y, scanner.w, scanner.d
   fakeLevels[scanner.y] = scanner.getRepr(1)
   scanner = nil
-  scanner = scan.init(x,z,ny,w,d,1)
+  scanner = scan.init(x,z,y+dy,w,d,1)
   by=1
 end
 local function forceShift(dy)
@@ -69,7 +68,7 @@ local function forceShift(dy)
   by=by+dy
   normalize()
   if by ~= oby then return end
-  activate(by+dy)
+  activate(dy)
 end
 local function toggleMode()
   if scanMode == "none" then scanMode = "window"
