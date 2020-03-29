4b54d86d54d2524c8b1b53a3803d5a5d8c55c5cc
burlada <burlada@yandex-team.ru>
2020-03-29 22:25:37 +0300

    fix

--- misc/bin/scanner.lua
+++ misc/bin/scanner.lua
@@ -54,7 +54,7 @@ local function draw(tick)
   local repr = scanner.getRepr(by,bx,bz,scanW,scanD)
   local scans, sizes = scanner.getScanCnt()
   local cx,cz,cy = scanner.x+(bx-1+scanW/2)*scanner.bw, scanner.z+(bz-1+scanD/2)*scanner.bd, scanner.y+(by-1)*scanner.bh
-  local selfX, selfZ = -scanner.x-(bx-1)*scanner.bw, -scanner.z-(bz-1)*scanner.bd
+  local selfX, selfZ = 1-scanner.x-(bx-1)*scanner.bw, 1-scanner.z-(bz-1)*scanner.bd
   
   for z, line in ipairs(repr) do gpu.set(1, z, line) end
   if selfX >= 1 and selfZ >= 1 and selfX <= scanW*8 and selfZ <= scanD*8 and showSelf then gpu.set(selfX, selfZ, "@") end
